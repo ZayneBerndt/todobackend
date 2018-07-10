8ee82eed9a287aca28594a6687adbb94bb3a3afd
Zayne  Berndt <berndt.zayne@gmail.com>
2018-07-10 23:44:01 +0100

    build python todo API

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CORS Settings
+CORS_ORIGIN_ALLOW_ALL = True
